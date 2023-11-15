4492f29f4e4bca1cd4100d1aef8acb0c241e6144
khant-min <kmin01405@gmail.com>
2023-11-15 19:13:12 +0630

    first commit

---
+++ src/main/java/com/km/lms/LibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.km.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/km/lms/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
